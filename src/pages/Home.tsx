229463e6904c01fc63c03193b4898d859565762b
ThoriqW <thoriqwajedi88@gmail.com>
2024-12-27 15:26:16 +0800

    update url

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -21,6 +21,7 @@ const Home = () => {
       console.log(e);
     }
   }, [id]);
+
   return (
     <div className="flex flex-col justify-center min-h-screen items-center">
       <p className="mb-5 text-center">
@@ -30,7 +31,7 @@ const Home = () => {
       </p>
       <a
         target="_blank"
-        href={`https://berkastte.rssindhutrisnopalu.com/webapps/${location}/${typefile}/${filename}`}
+        href={`https://berkastte_new.rssindhutrisnopalu.com/${location}/${typefile}/${filename}`}
         type="button"
         className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
       >
